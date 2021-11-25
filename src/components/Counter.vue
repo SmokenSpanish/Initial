4452a3da6491cbad26d4bf8b3df1461cb062fb8d
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-25 18:30:46 +0300

    feat: start Counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="root">
+      
+  </div>
+</template>
+
+<script>
+
+
+export default {
+name: 'counter',
+
+data() {
+    return {
+
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
