9555dedfa60b9e8f2f77722e3b0adfc85309f4bc
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-26 18:23:32 +0300

    feat: ProfileCard error

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,34 +1,94 @@
 <template>
   <div class="user__card">
+    <h1>БАБКИ</h1>
     <div class="main-info">
-      <img src="../assets/1603344816_1-p-kris-khemsvort-2.jpg" width="500px" />
+      <img
+        class="card__image"
+        src="../assets/1603344816_1-p-kris-khemsvort-2.jpg"
+      />
       <div class="info">
-        <h2></h2>
+        <h2>
+            {{ getFullNameAuthor }}
+        </h2>
         <strong>Бизнес-коуч</strong>
         <ul>
-          <li>Учу людей делать РЕАЛЬНЫЕ бабки, на себя просто не хватило времени X(</li>
+          <li>
+            Учу людей делать РЕАЛЬНЫЕ бабки, на себя просто не хватило времени
+            X(
+          </li>
           <li>Не знаю, что такое "Успешный Успех", но знаю, что нужно ТЕБЕ!</li>
           <li>Пообещал, что побреюсь, когда кого нибудь ОБМАНУ.</li>
         </ul>
       </div>
     </div>
     <p>Участников: 0</p>
-    <ul></ul>
+    <ul>
+        <li v-for="{users, index} in users" :key="index">
+        {{ getFullNameUser(users) }}
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'userCard',
+  name: "profileCard",
   data() {
     return {
-        firstName: 'Игнатий',
-        secondName: 'Иларионович',
-        lastName: 'Богатов'
+      firstName: 'Игнатий',
+      secondName: 'Иларионович',
+      lastName: 'Богатов',
+      users: [
+          {
+            firstName: 'Иван',
+            secondName: 'Иванович',
+            lastName: 'Иванов'
+          },
+          {
+            firstName: 'Иван',
+            secondName: 'Иванович',
+            lastName: 'Иванов'
+          },
+          {
+            firstName: 'Иван',
+            secondName: 'Иванович',
+            lastName: 'Иванов'
+          },
+      ]
     };
   },
+  computed: {
+    getFullNameAuthor() {
+        return `${this.firstName} ${this.secondName} ${this.lastName}`.toUpperCase()
+    }
+  },
+  methods: {
+      getFullNameUser(user) {
+          return user.secondName
+      }
+  }
 };
 </script>
 
 <style>
+.user__card {
+  padding: 15px;
+  border: 1px solid #555;
+}
+
+.card__image {
+  height: 500px;
+  object-fit: cover;
+  margin-right: 20px;
+}
+
+ul {
+    list-style: none;
+    padding: 0;
+}
+
+.main-info {
+    display: flex;
+    line-height: 2.1em;
+}
 </style>
