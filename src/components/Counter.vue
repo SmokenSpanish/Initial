ec3e7a1b20b8a772a1327d4f7a6f767d0a520561
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-26 16:14:04 +0300

    feat: ProfileCard started

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,23 +1,60 @@
 <template>
-  <div class="root">
-      
+  <div class="counter__container">
+    <button class="btn__counter btn__counter-minus" 
+    @click="decreaseCount" 
+    type="button">-</button>
+    <input class="input__counter" 
+    type="number" 
+    :value="counter" :min="minCount" :max="maxCount" />
+    <button class="btn__counter btn__counter-plus" @click="increaseCount">+</button>
   </div>
 </template>
 
 <script>
-
-
 export default {
-name: 'counter',
+  name: "counter",
 
-data() {
+  data() {
     return {
+      counter: 1,
+      minCount: 1,
+      maxCount: 10
+    };
+  },
 
+  methods: {
+    increaseCount() {
+      if(this.counter < this.maxCount) {
+        this.counter++
+      }
+    },
+
+    decreaseCount() {
+      if(this.counter > this.minCount) {
+        this.counter--
+      }
     }
-}
-}
+  }
+};
 </script>
 
 <style>
+button {
+  padding: 0;
+}
+.counter__container {
+  display: flex;
+}
+
+.btn__counter {
+  width: 20px;
+  height: 20px;
+}
+
+.input__counter {
+  padding: 0;
+  width: 35px;
+  height: 20px;
+}
 
 </style>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="user__card">
+    <div class="main-info">
+      <img src="../assets/1603344816_1-p-kris-khemsvort-2.jpg" width="500px" />
+      <div class="info">
+        <h2></h2>
+        <strong>Бизнес-коуч</strong>
+        <ul>
+          <li>Учу людей делать РЕАЛЬНЫЕ бабки, на себя просто не хватило времени X(</li>
+          <li>Не знаю, что такое "Успешный Успех", но знаю, что нужно ТЕБЕ!</li>
+          <li>Пообещал, что побреюсь, когда кого нибудь ОБМАНУ.</li>
+        </ul>
+      </div>
+    </div>
+    <p>Участников: 0</p>
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'userCard',
+  data() {
+    return {
+        firstName: 'Игнатий',
+        secondName: 'Иларионович',
+        lastName: 'Богатов'
+    };
+  },
+};
+</script>
+
+<style>
+</style>
