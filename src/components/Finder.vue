8623d951e63e6512ef678365d1f95fde6f4f19f5
Maksim Basov <maksimbasov1992@mail.ru>
2021-11-30 17:01:34 +0300

    feat Finder fin

--- src/components/Finder.vue
+++ src/components/Finder.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="finder">
       <h1 class="finder__title">Я найду тебя!</h1>
-      <input type="text">
+      <input type="text" @input="searchText = $event.target.value">
       <p class="finder__text">Всего имен:{{ ` ${names.length}` }}</p> 
-      <p class="finder__text">Всего совпадений:</p>
+      <p class="finder__text">Всего совпадений:{{ getFilteredNames.length }}</p>
   </div>
 <ul>
-    <li :value="names">
-        {{  }}
+    <li v-for="(name, index) in getFilteredNames" :key="index">
+        {{ name }}
     </li>
 </ul>
 </template>
@@ -16,7 +16,13 @@
 export default {
     data() {
         return {
-            names: ['Василий', 'Николай', 'Максим', 'Раф']
+            searchText: '',
+            names: ['Василий', 'Николай', 'Максим', 'Раф', 'Зинаида']
+        }
+    },
+    computed: {
+        getFilteredNames() {
+            return this.names.filter(name => name.toLowerCase().includes(this.searchText.toLowerCase()))
         }
     }
     
